3097b5c3c2b0757a12b69b7ec122b49c95004206
linder3hs <linderhassinger00@gmail.com>
2022-08-25 14:58:36 -0500

    add cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import express from "express";
+// const cors = require("cors")
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(cors());
 
 const movies = [
   {
